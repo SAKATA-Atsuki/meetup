aa756057eb76c01254d4fe12d77dc06c2ffe30fe
SAKATA-Atsuki <hhnkkr@icloud.com>
2021-02-02 16:24:15 +0900

    update

--- database/migrations/2021_02_02_131011_create_favorites_table.php
+++ database/migrations/2021_02_02_131011_create_favorites_table.php
@@ -15,7 +15,7 @@ class CreateFavoritesTable extends Migration
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->increments('id')->comment('お気に入りID');
-            $table->integer('freshman_id')->nullable($value = true)->comment('新入生ID');
+            $table->integer('freshman_id')->comment('新入生ID');
             $table->integer('circle_id')->comment('サークルID');
             $table->timestamp('created_at')->nullable($value = true)->comment('登録日時');
             $table->timestamp('updated_at')->nullable($value = true)->comment('編集日時');
