7c4385c5145f85ad23b5c4625615a8703c1260c4
SAKATA-Atsuki <hhnkkr@icloud.com>
2021-02-03 13:58:48 +0900

    update

--- storage/framework/views/017c43c6f795089285b5eeb0489a766386806028.php
+++ storage/framework/views/017c43c6f795089285b5eeb0489a766386806028.php
@@ -65,15 +65,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <?php if(Route::has('login')): ?>
+            <?php if(Route::has('user.login')): ?>
                 <div class="top-right links">
                     <?php if(auth()->guard()->check()): ?>
                         <a href="<?php echo e(url('/home')); ?>">Home</a>
                     <?php else: ?>
-                        <a href="<?php echo e(route('login')); ?>">Login</a>
+                        <a href="<?php echo e(route('user.login')); ?>">Login</a>
 
-                        <?php if(Route::has('register')): ?>
-                            <a href="<?php echo e(route('register')); ?>">Register</a>
+                        <?php if(Route::has('user.register')): ?>
+                            <a href="<?php echo e(route('user.register')); ?>">Register</a>
                         <?php endif; ?>
                     <?php endif; ?>
                 </div>
