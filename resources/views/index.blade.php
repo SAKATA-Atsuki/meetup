5c3ed4693c2bb8282253e624b64cca0e7fcd11df
SAKATA-Atsuki <hhnkkr@icloud.com>
2021-02-25 10:28:37 +0900

    Google Search Consoleの手続き

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="google-site-verification" content="0fKXNAUFii0sAWjxThf7uocRVAxGZD507XQhKypEXKY" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
     <script type="text/javascript">
