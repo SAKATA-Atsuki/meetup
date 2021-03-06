4dc70047c26f93ff104f7fd6913ba8996a9d76ff
SAKATA-Atsuki <hhnkkr@icloud.com>
2021-03-06 21:58:03 +0900

    ユーザ全体へのお知らせ

--- app/Http/Controllers/AnnounceController.php
+++ app/Http/Controllers/AnnounceController.php
@@ -13,9 +13,9 @@ class AnnounceController extends Controller
     // アナウンス
     public function index()
     {
-        foreach (Freshman::all() as $freshman) {
-            Mail::to($freshman['email'])->send(new AnnounceNotification($freshman['name_sei'] . $freshman['name_mei']));
-        }
+        // foreach (Freshman::all() as $freshman) {
+        //     Mail::to($freshman['email'])->send(new AnnounceNotification($freshman['name_sei'] . $freshman['name_mei']));
+        // }
         
         foreach (Circle::all() as $circle) {
             Mail::to($circle['email'])->send(new AnnounceNotification($circle['name']));
